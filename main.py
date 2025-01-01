554c5c79f25b51e2f955621826a90b567b63f305
tanzidm627 <168069694+tanzidm627@users.noreply.github.com>
2025-01-01 18:34:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from proxyall import *
 from vipp import checkvip
 from random_user import geninfo
 from mchk import new_func,get_response_mchk
-tok = "8104191497:AAEOLdImuTIn_7RNoZW49STbdqxMwWns8cQ"
+tok = "6630428239:AAGEcfimvRQEWP0-mueuLMxPCPgo2mLJLUo"
 bot=telebot.TeleBot(tok)
 @bot.message_handler(commands=['start'])
 def run(message):
